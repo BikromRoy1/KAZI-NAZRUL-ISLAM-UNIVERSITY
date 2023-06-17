978e48cd5cc4938a9fad36bb844db97d10d913ab
BikromRoy1 <bikrom@peoplentech.com.bd>
2023-06-17 18:57:31 +0600

    update slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select('#hero-carousel-indicators');
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true);
+
+  heroCarouselItems.forEach((item, index) => {
+    index === 0
+      ? (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
+          index +
+          "' class='active'></li>")
+      : (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
+          index +
+          "'></li>");
+  });
 
   /**
    * Initiate glightbox
