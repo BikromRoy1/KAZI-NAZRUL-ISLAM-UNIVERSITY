b0e7f1954d866ab081290d9bf93f1267cc37e990
BikromRoy1 <bikrom@peoplentech.com.bd>
2023-07-12 13:41:15 +0600

    count up edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,65 +193,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox',
-  });
-
-  /**
    * Initiate Pure Counter
    */
   new PureCounter();
 
   /**
-   * Init isotope layout and filters
-   */
-  function initIsotopeLayout() {
-    document
-      .querySelectorAll('.isotope-layout')
-      .forEach(function (isotopeItem) {
-        let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-        let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-        let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
-
-        let initIsotope = new Isotope(
-          isotopeItem.querySelector('.isotope-container'),
-          {
-            itemSelector: '.isotope-item',
-            layoutMode: layout,
-            filter: filter,
-            sortBy: sort,
-          }
-        );
-
-        isotopeItem
-          .querySelectorAll('.isotope-filters li')
-          .forEach(function (filters) {
-            filters.addEventListener(
-              'click',
-              function () {
-                isotopeItem
-                  .querySelector('.isotope-filters .filter-active')
-                  .classList.remove('filter-active');
-                this.classList.add('filter-active');
-                initIsotope.arrange({
-                  filter: this.getAttribute('data-filter'),
-                });
-                if (typeof aosInit === 'function') {
-                  aosInit();
-                }
-              },
-              false
-            );
-          });
-      });
-  }
-  window.addEventListener('load', initIsotopeLayout);
-
-
-  /**
-   * Testimonials slider
+   * Research Activities slider
    */
   new Swiper('.activities-slider', {
     speed: 600,
